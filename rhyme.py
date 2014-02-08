21a2ab9eda2a6c21ab5916cfebbf7cfbfc26b01c
Daniel Piet <dpiet@hubspot.com>
2014-02-07 22:06:33 -0500

    oops namespace

---
+++ rhyme.py
@@ -0,0 +1,16 @@
+import nltk
+from nltk.corpus import cmudict
+
+def rhyme(inp, level):
+	entries = cmudict.entries()
+	syllables = [(word, syl) for word, syl in entries if word == inp]
+	rhymes = []
+	for (word, syllable) in syllables:
+		rhymes += [word for word, pron in entries if pron[-level:] == syllable[-level:]]
+	return set(rhymes)
+
+print "word?"
+word = raw_input()
+print "level?"
+level = input()
+print rhyme(word, level)
